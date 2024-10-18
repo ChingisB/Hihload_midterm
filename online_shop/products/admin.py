f3d8cc355a28ce132363ae9ec566eb83cc691a1d
chingis <chingis.bagdatov@gmail.com>
2024-10-18 09:39:16 +0500

    add admin views

--- online_shop/products/admin.py
+++ online_shop/products/admin.py
@@ -1,3 +1,41 @@
-from django.contrib import admin
+# admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Product, Category, Inventory, ProductCategory
+
+
+class ProductCategoryInline(admin.TabularInline):
+    model = ProductCategory
+    extra = 1
+
+
+class InventoryInline(admin.StackedInline):
+    model = Inventory
+    extra = 0
+    readonly_fields = ['count']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'price']
+    search_fields = ['name', 'description']
+    inlines = [ProductCategoryInline, InventoryInline]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+    search_fields = ['name']
+
+
+@admin.register(Inventory)
+class InventoryAdmin(admin.ModelAdmin):
+    list_display = ['product', 'count']
+    search_fields = ['product__name']
+    readonly_fields = ['product']
+
+
+@admin.register(ProductCategory)
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = ['product', 'category']
+    search_fields = ['product__name', 'category__name']
