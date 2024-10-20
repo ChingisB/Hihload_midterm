ca775813ff7ac4e9b2f909354b2df4121cf913fb
chingis <chingis.bagdatov@gmail.com>
2024-10-20 18:19:56 +0500

    add cache

--- online_shop/orders/views.py
+++ online_shop/orders/views.py
@@ -2,7 +2,8 @@ from rest_framework import generics, serializers, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .models import Customer, Order, ShippingAddress
-from .serializers import CustomerSerializer, OrderSerializer, OrderItemSerializer, ShippingAddressSerializer
+from .serializers import CustomerSerializer, OrderSerializer, ShippingAddressSerializer
+from django.core.cache import cache
 
 
 class CustomerCreateView(generics.CreateAPIView):
@@ -32,15 +33,31 @@ class OrderInfoView(generics.APIView):
 
     def get(self, request, order_id=None):
         if order_id:
+
+            cache_key = f'order_{order_id}'
+            cached_order = cache.get(cache_key)
+
+            if cached_order:
+                return Response(cached_order, status=status.HTTP_200_OK)
+            
             try:
                 order = Order.objects.get(id=order_id, customer__user=request.user)
                 serializer = OrderSerializer(order)
+                cache.set(cache_key, serializer.data, timeout=60*5) 
                 return Response(serializer.data, status=status.HTTP_200_OK)
             except Order.DoesNotExist:
                 return Response({"detail": "Order not found."}, status=status.HTTP_404_NOT_FOUND)
         else:
+            cache_key = 'orders'
+            cached_orders = cache.get(cache_key)
+
+            if cached_orders:
+                return Response(cached_orders, status=status.HTTP_200_OK)
+            
             orders = Order.objects.filter(customer__user=request.user)
+            
             serializer = OrderSerializer(orders, many=True)
+            cache.set(cache_key, serializer.data, timeout=60*5)
             return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
@@ -51,6 +68,9 @@ class OrderInfoView(generics.APIView):
             total_amount = sum(item.get_total_price() for item in order.items.all())
             order.total_amount = total_amount
             order.save()
+        
+            cache_key = 'orders'
+            cache.delete(cache_key)
 
             return Response(OrderSerializer(order).data, status=status.HTTP_201_CREATED)
         return Response(order_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -69,6 +89,9 @@ class OrderInfoView(generics.APIView):
             updated_order.total_amount = total_amount
             updated_order.save()
 
+            cache_key = f'order_{updated_order.id}'
+            cache.delete(cache_key)
+
             return Response(OrderSerializer(updated_order).data, status=status.HTTP_200_OK)
         return Response(order_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -76,6 +99,8 @@ class OrderInfoView(generics.APIView):
         try:
             order = Order.objects.get(id=order_id, customer__user=request.user)
             order.delete()
+            cache_key = 'orders'
+            cache.delete(cache_key)
             return Response({"detail": "Order deleted."}, status=status.HTTP_204_NO_CONTENT)
         except Order.DoesNotExist:
             return Response({"detail": "Order not found."}, status=status.HTTP_404_NOT_FOUND)
@@ -84,7 +109,6 @@ class OrderInfoView(generics.APIView):
 class ShippingAddressCreateView(generics.CreateAPIView):
     serializer_class = ShippingAddressSerializer
     permission_classes = [IsAuthenticated]
-
 
     def perform_create(self, serializer):
         return serializer.create(user=self.request.user)
--- online_shop/products/views.py
+++ online_shop/products/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics, status
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
 from .models import Product, Inventory, Category
 from .serializers import CategorySerializer, InventorySerializer, ProductSerializer
 
@@ -9,20 +10,36 @@ class CategoryListCreateView(generics.ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
+    @method_decorator(cache_page(60 * 5))
+    def get(self, request):
+        return super().get(request)
+
 
 class CategoryDetailView(generics.RetrieveAPIView):
     queryset = Category.objects.all()
-    serializer_class =  CategorySerializer
+    serializer_class = CategorySerializer
+
+    @method_decorator(cache_page(60 * 5))
+    def get(self, request):
+        return super().get(request)
 
 
 class ProductListCreateView(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
+    @method_decorator(cache_page(60 * 5))
+    def get(self, request):
+        return super().get(request)
+
 
 class ProductDetailView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    @method_decorator(cache_page(60 * 5))
+    def get(self, request):
+        return super().get(request)
 
 
 # It's a custom view. So it's for retrieving specific products inventory
@@ -37,3 +54,7 @@ class ProductInventoryView(generics.RetrieveAPIView):
             return self.get_queryset().get()
         except Inventory.DoesNotExist:
             return Response({"detail": "Inventory not found."}, status=status.HTTP_404_NOT_FOUND)
+    
+    @method_decorator(cache_page(60 * 5))
+    def get(self, request):
+        return super().get(request)
